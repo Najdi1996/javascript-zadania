4a5c9b5b6b89263ede43941c4a5048f87f728664
Najdi1996 <74015149+Najdi1996@users.noreply.github.com>
2021-02-05 23:23:39 +0100

    Update script.js

---
+++ 1.Początki/script.js
@@ -0,0 +1,12 @@
+//Zad 1
+console.log("Dominik");
+
+//Zad 2
+console.log(12+32);
+console.log(52/16); // zmiennoprzecinkowa
+console.log(5*2);
+
+//Zad 3
+console.log("Hello World");
+//console.log(Hello World); // bład, nieznana zmienna
+//console.log("Hello World); // bład, niezakończony string
