5328eb747d8e17aaf64806c1ca34001d5a888c8b
Najdi1996 <74015149+Najdi1996@users.noreply.github.com>
2021-02-05 23:25:23 +0100

    Update script.js

---
+++ 3.Operatory/script.js
@@ -0,0 +1,48 @@
+//Zad 1
+
+const firstValue = false
+const secondValue = true
+console.log(firstValue === secondValue)
+
+//Zad 2
+
+let first = 4
+let second = 2 
+let moduloResult = 0
+
+moduloResult = first % second
+console.log(moduloResult);
+
+//Zad 3
+
+let first = 'first'
+let second = 'second'
+let result = ''
+result = first + second
+console.log(result)
+
+//Zad 4
+
+var someNumber = 425;
+var someString = '425';
+
+console.log(someNumber == someString) //konwersja typów
+console.log(someNumber === someString) // inny typ zmiennych
+
+//Zad 5
+
+let counter = 30
+console.log(counter)
+counter++
+console.log(counter)
+counter--
+console.log(counter)
+
+
+//Zad 6
+
+let firstNumber = 2
+let secondNumber = 4
+let result = null
+result = firstNumber>secondNumber
+console.log(result)
